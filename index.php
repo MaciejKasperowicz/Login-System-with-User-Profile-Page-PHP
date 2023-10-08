7e296f2009aff993ad1edca572235792c2879321
MaciejKasperowicz <makasp@wp.pl>
2023-10-08 09:54:42 +0200

    Add basic form

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="index-login">
+        <div class="wrapper">
+            <div class="index-login-signup">
+                <h4>SIGN UP</h4>
+                <p>Don't have an account yet? Sign up here!</p>
+                <form action="includes/signup.php" method="post">
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="text" name="password" placeholder="Password">
+                    <input type="text" name="passwordRepeat" placeholder="Repeat Password">
+                    <input type="text" name="email" placeholder="E-mail">
+                    <br>
+                    <button type="submit" name="submit">SIGN UP</button>
+                </form>
+            </div>
+            <div class="index-login-login">
+                <h4>LOGIN</h4>
+                <p>Login here!</p>
+                <form action="includes/login.php" method="post">
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="text" name="password" placeholder="Password">
+                    <br>
+                    <button type="submit" name="submit">LOGIN</button>
+                </form>
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
